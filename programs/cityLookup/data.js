463b3442207295db60b75241e58c27fde9134b1b
amcneil36 <51429671+amcneil36@users.noreply.github.com>
2021-01-18 00:49:22 -0800

    Add files via upload

--- programs/cityLookup/data.js
+++ programs/cityLookup/data.js
@@ -1,3 +1,24 @@
+var augustHigh = "August high (F)";
+var decHigh = "December high (F)";
+var annualRainfall = "Annual rainfall (in)";
+var arrayOfFields = [augustHigh,decHigh, annualRainfall];
+
+function createMap(obj){
+  let map = new Map();
+  map.set(augustHigh, obj.augHi);
+  map.set(decHigh, obj.decHi);
+  map.set(annualRainfall, obj.numInchesOfRain);
+  return map;
+}
+
+function insertColumnBodies(tr, obj){
+  insertColumnBody(tr, obj.cityName);
+  insertColumnBody(tr, obj.stateName);
+  insertColumnBody(tr, obj.augHi);
+  insertColumnBody(tr, obj.decHi);
+  insertColumnBody(tr, obj.numInchesOfRain);	
+}
+
 class Data {
   constructor(cityName, stateName, augHi, decHi, numInchesOfRain) {
     this.cityName = cityName;
--- programs/cityLookup/main.js
+++ programs/cityLookup/main.js
@@ -1,24 +1,3 @@
-var augustHigh = "August high (F)";
-var decHigh = "December high (F)";
-var annualRainfall = "Annual rainfall (in)";
-var arrayOfFields = [augustHigh,decHigh, annualRainfall];
-
-function createMap(obj){
-  let map = new Map();
-  map.set(augustHigh, obj.augHi);
-  map.set(decHigh, obj.decHi);
-  map.set(annualRainfall, obj.numInchesOfRain);
-  return map;
-}
-
-function insertColumnBodies(tr, obj){
-  insertColumnBody(tr, obj.cityName);
-  insertColumnBody(tr, obj.stateName);
-  insertColumnBody(tr, obj.augHi);
-  insertColumnBody(tr, obj.decHi);
-  insertColumnBody(tr, obj.numInchesOfRain);	
-}
-
 var idx = 0;
 addRow();
 
