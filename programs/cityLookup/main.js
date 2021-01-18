32bd7a2793312bf519cdc6c39e8993f89da5d19c
amcneil36 <51429671+amcneil36@users.noreply.github.com>
2021-01-17 20:30:39 -0800

    Add files via upload

--- programs/cityLookup/main.js
+++ programs/cityLookup/main.js
@@ -1,42 +1,100 @@
 //alert (arr[0].siteName); // alerts `#fff`
 
-function main(){
-	var outputString = "";
-	var fieldId = document.getElementById("fieldName").value;
+var idx = 0;
+addRow();
+function addRow(){
+
+createFirstElement();
+createSecondElement();
+createThirdElement();
+var myDiv = document.getElementById("extraRows");
+var f = document.createElement('br');
+myDiv.appendChild(f);
+idx++;
+}
+
+function createThirdElement(){
+	var input = document.createElement("input");
+input.setAttribute('type', 'text');
+input.setAttribute('size', '1');
+input.setAttribute('id', 'value' + idx);
+  var myDiv = document.getElementById("extraRows");
+    myDiv.appendChild(input);
+}
+
+function createSecondElement(){
+  var myDiv = document.getElementById("extraRows");
+  var array = [">", ">=", "<", "<=", "="];
+  var selectList = document.createElement("select");
+  selectList.setAttribute("id", "symbols" + idx);
+  myDiv.appendChild(selectList);
+
+  for (var i = 0; i < array.length; i++) {
+    var option = document.createElement("option");
+    option.setAttribute("value", array[i]);
+    option.text = array[i];
+    selectList.appendChild(option);
+  }	
+}
+
+function createFirstElement(){
+  var myDiv = document.getElementById("extraRows");
+  var array = ["August high","December high"];
+  var selectList = document.createElement("select");
+  selectList.setAttribute('id', 'fieldName' + idx);
+  myDiv.appendChild(selectList);
+
+  for (var i = 0; i < array.length; i++) {
+    var option = document.createElement("option");
+    option.setAttribute("value", array[i]);
+    option.text = array[i];
+    selectList.appendChild(option);
+  }	
+}
+
+function doesObjQualify(obj){
+	for (var i = 0; i < idx; i++){
+		var fieldId = document.getElementById("fieldName" + i).value;
 	var fieldName = null;
 	
-		var value = parseFloat(document.getElementById("value").value);
-	
-	var symbol = document.getElementById("symbols").value;
-	
-	for (var i = 0; i < arr.length; i++){
-		 	if (fieldId == "augustHi"){
-	   fieldName = arr[i].augHi;
+    var value = parseFloat(document.getElementById("value" + i).value);
+	var symbol = document.getElementById("symbols" + i).value;
+	if (fieldId == "August high"){
+	    fieldName = obj.augHi;
 	}
 	else{
-	   fieldName = arr[i].decHi;
+	    fieldName = obj.decHi;
 	}
-	
 	var flag = false;
-	if (symbol == "greaterThan"){
+	if (symbol == ">"){
 		flag = fieldName > value;
 	}
-	else if (symbol == "greaterThanOrEqualTo"){
+	else if (symbol == ">="){
 	  flag = fieldName >= value;	
 	}
-	else if (symbol == "lessThan"){
+	else if (symbol == "<"){
 	  flag = fieldName < value;	
 	}
-	else if (symbol == "lessThanOrEqualTo"){
+	else if (symbol == "<="){
 	  flag = fieldName <= value;	
 	}
-	else if (symbol == "equal"){
+	else if (symbol == "="){
 	  flag = fieldName == value;	
 	}
-    if (flag){
-		outputString += arr[i].siteName + "<br>";
+    if (flag == false){
+     return false;		
 	}
+    }
+	return true;
+}
+
+function main(){
+	var outputString = "";
+	
+	for (var i = 0; i < arr.length; i++){
+      if (doesObjQualify(arr[i])){
+		outputString += arr[i].siteName + ", aug_hi = " + arr[i].augHi + ", dec_hi = " + arr[i].decHi + "<br>";
+	  }
 	}
 	document.getElementById("output").innerHTML  = outputString;
-	//alert(document.getElementById("fieldName").value);
 }
