eee3dbf1919703a8a32bf7e40515ed318bc4b0d7
amcneil36 <andrewemcneil@gmail.com>
2021-09-21 12:36:05 -0700

    savingg work

--- programs/com.hey/src/main/java/com/hey/SexOffenderStats.java
+++ programs/com.hey/src/main/java/com/hey/SexOffenderStats.java
@@ -2,24 +2,19 @@ package main.java.com.hey;
 
 public class SexOffenderStats extends CityStats{
 
-	private static final String SEARCH_STRING = "The ratio of all residents to sex offenders in ";
-	private static final String SEARCH_STRING2 = " is ";
+	private static final String SEARCH_STRING = " a ratio of ";
 	
 	@Override
 	protected void updateData(Data data, String stateName) throws Exception {
 		String city = data.cityName.replace(" ", "-");
-		String url = "https://www.city-data.com/so/so-" + city + "-" + stateName + ".html";
+		// https://www.homefacts.com/offenders/Texas/Nueces-County/Corpus-Christi.html
+		String url = "https://www.homefacts.com/offenders/" + stateName + "/" + data.countyName.replace(" ", "-")  + "/" + city + ".html";
 		String text = Util.ReadTextFromPage(url);
 		if (!text.contains(SEARCH_STRING)) {
 			return;
 		}
 		int startIdx = text.indexOf(SEARCH_STRING)+SEARCH_STRING.length();
-		text = text.substring(startIdx);
-		if (!text.contains(SEARCH_STRING2) || !text.contains(".")) {
-			return;
-		}
-		startIdx = text.indexOf(SEARCH_STRING2)+SEARCH_STRING2.length();
-		text = text.substring(startIdx, text.indexOf(".")).replace(",", "");
+		text = text.substring(0, text.indexOf(" "));
 		System.out.println(text);
 		data.sexOffenderCount = text;
 	}
