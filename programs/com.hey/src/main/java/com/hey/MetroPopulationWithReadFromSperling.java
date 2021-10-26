25422366e4c859027c67f705a6582d4a07f6d196
amcneil36 <andrewemcneil@gmail.com>
2021-10-25 20:43:53 -0700

    f

--- programs/com.hey/src/main/java/com/hey/MetroPopulationWithReadFromSperling.java
+++ programs/com.hey/src/main/java/com/hey/MetroPopulationWithReadFromSperling.java
@@ -8,11 +8,12 @@ import main.java.com.hey.CityStats.Data;
 
 public class MetroPopulationWithReadFromSperling extends CityStats {
 
-	
 	static Map<String, Integer> mapOfMetroToPopulation = new HashMap<>();
+
 	// maybe write map values to a file? but multithreading might be an issue
 	// multithreading shouldn't be an issue because fillMap is called first.
-	// write to file after each add to the map. so if we crash, we don't lose our work
+	// write to file after each add to the map. so if we crash, we don't lose our
+	// work
 	private static void fillMap() throws Exception {
 		List<Data> dataList = CreateBigCsv.readInput();
 		for (Data data : dataList) {
@@ -23,31 +24,35 @@ public class MetroPopulationWithReadFromSperling extends CityStats {
 			if (mapOfMetroToPopulation.containsKey(key)) {
 				continue;
 			}
-			else {
-				System.out.println(key);
-				mapOfMetroToPopulation.put(key, 0);
-			}
+			int population = getPopulationForMetro(key);
+			mapOfMetroToPopulation.put(key, population);
 		}
 	}
-	
-	
-	private static String getKey(Data data) {
-		// TODO Auto-generated method stub
-		return data.stateName.replace(" ", "_").toLowerCase() + data.metro.replace(" ", "_").toLowerCase();
+
+	private static int getPopulationForMetro(String key) {
+		String url = "https://www.bestplaces.net/metro/" + key;
+		String text = Util.ReadTextFromPage(url);
+		text = text.substring(text.indexOf(" population is ") + " population is ".length());
+		text = text.substring(0, text.indexOf(" people"));
+		text = text.replace(",", "");
+		return Integer.valueOf(text);
 	}
 
+	private static String getKey(Data data) {
+		return data.stateName.replace(" ", "_").toLowerCase() + "/" + data.metro.replace(" ", "_").toLowerCase();
+	}
 
 	public static void main(String[] args) throws Exception {
 		fillMap();
 		MetroPopulation s = new MetroPopulation();
-		//s.processAllStates();
+		// s.processAllStates();
 
 	}
 
 	@Override
 	protected void updateData(Data data, String stateName) throws Exception {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 }
