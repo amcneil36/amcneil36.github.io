4d12b4ecd71c55eaa2a49640e6f0c928893fdb62
amcneil36 <andrewemcneil@gmail.com>
2021-11-24 11:40:30 -0800

    fixing bug where cities in the non dominant state of a metro will have the wrong metro population

--- programs/com.hey/src/main/java/com/hey/MetroPopulationWithReadFromSperling.java
+++ programs/com.hey/src/main/java/com/hey/MetroPopulationWithReadFromSperling.java
@@ -10,6 +10,58 @@ public class MetroPopulationWithReadFromSperling extends CityStats {
 
 	static Map<String, Integer> mapOfMetroToPopulation = new HashMap<>();
 
+	// maybe write map values to a file? but multithreading might be an issue
+	// multithreading shouldn't be an issue because fillMap is called first.
+	// write to file after each add to the map. so if we crash, we don't lose our
+	// work
+	private static void fillMap() throws Exception {
+		List<Data> dataList = CreateBigCsv.readInput();
+		for (Data data : dataList) {
+			if (data.metro.equals("None")) {
+				System.out.println("hi");
+				continue;
+			}
+			String key = getKey(data);
+			if (mapOfMetroToPopulation.containsKey(key)) {
+				continue;
+			}
+			int population = getPopulationForMetro(key, data);
+			if (population == -1) {
+				badMetrosFound.add(getNewKey(data));
+				badMetrosWithMetroNameOnly.add(data.metro);
+			}
+			mapOfMetroToPopulation.put(key, population);
+		}
+	}
+
+	private static int getPopulationForMetro(String key, Data data) {
+		String url = "https://www.bestplaces.net/metro/" + key;
+		String text = Util.ReadTextFromPage(url);
+		String originalText = text;
+		if (!text.contains("s population is ")) {
+			return -1;
+		}
+		text = text.substring(text.indexOf("s population is ") + "s population is ".length());
+		if (!text.contains(" people")) {
+			return -1;
+		}
+		text = text.substring(0, text.indexOf(" people"));
+		text = text.replace(",", "");
+		try {
+			return Integer.valueOf(text);
+		} catch (Exception ex) {
+			System.out.println("nothing found for: " + url);
+			System.out.println("original text: " + originalText);
+			throw new RuntimeException("hi");
+		}
+	}
+
+	private static String getKey(Data data) {
+		return data.stateName.replace(" ", "_").toLowerCase() + "/" + data.metro.replace(" ", "_").toLowerCase();
+	}
+	
+	/////////////////////
+
 	public static String getNewKey(Data data) {
 		return data.metro + "," + data.stateName;
 	}
@@ -18,7 +70,7 @@ public class MetroPopulationWithReadFromSperling extends CityStats {
 	static Set<String> badMetrosWithMetroNameOnly = new HashSet<>();
 	static Map<String, String> mapOfMetroNameToPopulation = new HashMap<>();
 
-	private static void fillMap() throws Exception {
+	private static void fillOtherMap() throws Exception {
 		List<Data> dataList = CreateBigCsv.readInput();
 		for (Data data : dataList) {
 			if (data.metro.equals("None")) {
@@ -43,9 +95,15 @@ public class MetroPopulationWithReadFromSperling extends CityStats {
 
 		}
 	}
+	
+	
+	
+	////////////////////// I have not ran this code since I fixed the bug that fixed the issue with the wrong metro
+	// population for cities in a metro but not in the dominant state. so should test that this main works
 
 	public static void main(String[] args) throws Exception {
 		fillMap();
+		fillOtherMap();
 		MetroPopulationWithReadFromSperling s = new MetroPopulationWithReadFromSperling();
 		s.processAllStates();
 
@@ -53,9 +111,22 @@ public class MetroPopulationWithReadFromSperling extends CityStats {
 
 	@Override
 	protected void updateData(Data data, String stateName) throws Exception {
+		if (data.metro.equals("None")) {
+			data.metroPopulation = "N/A";
+			return;
+		}
 		if (badMetrosFound.contains(getNewKey(data))) {
 			data.metroPopulation = mapOfMetroNameToPopulation.get(data.metro);
+			return;
 		}
+
+		String key = getKey(data);
+		if (!mapOfMetroToPopulation.containsKey(key)) {
+			System.out.println("key: " + key + " was not found.");
+			return;
+		}
+		data.metroPopulation = String.valueOf(mapOfMetroToPopulation.get(key));
+
 	}
 
 }
