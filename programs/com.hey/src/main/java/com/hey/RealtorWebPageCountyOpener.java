2d6e25701af26346b615844e097355eb90dcb10a
amcneil36 <andrewemcneil@gmail.com>
2022-03-04 23:30:03 -0800

    f

--- programs/com.hey/src/main/java/com/hey/RealtorWebPageCountyOpener.java
+++ programs/com.hey/src/main/java/com/hey/RealtorWebPageCountyOpener.java
@@ -11,7 +11,7 @@ import java.util.Set;
 public class RealtorWebPageCountyOpener {
 
 	private static boolean shouldTabBeOpened(CityStats.Data data) throws Exception {
-		return !data.metroPopulation.contains("N/A") && data.metro.contains("Sarasota") && data.stateName.equals("Florida") && Util.getDaysSinceLastUpdated(data) > 10;
+		return !data.metroPopulation.contains("N/A") && data.metro.contains("Vegas") && data.stateName.equals("Nevada") && Util.getDaysSinceLastUpdated(data) > 10;
 	}
 
 	private static Map<String, String> map = new HashMap<String, String>();
