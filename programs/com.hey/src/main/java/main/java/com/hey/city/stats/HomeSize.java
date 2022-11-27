94d205e782f8b2ebaf08b36609031ff7bcfa4567
amcneil36 <andrewemcneil@gmail.com>
2022-11-27 15:32:35 -0500

    about to run code

---
+++ programs/com.hey/src/main/java/main/java/com/hey/city/stats/HomeSize.java
@@ -0,0 +1,26 @@
+package main.java.com.hey.city.stats;
+
+import main.java.com.hey.CityStats;
+import main.java.com.hey.Util;
+
+public class HomeSize extends CityStats {
+
+	public static void main(String[] args) throws Exception {
+		CityStats cs = new HomeSize();
+		cs.processAllStates();
+
+	}
+
+	@Override
+	protected void updateData(Data data, String stateName) throws Exception {
+		data.homeSquareFeet = "N/A";
+		if (!data.costPerSquareFoot.equals("N/A") && !data.medianHomePrice.equals("N/A")) {
+			double cost = Integer.valueOf(data.costPerSquareFoot.replace("$", ""));
+			double costPerSqFt = Integer.valueOf(data.medianHomePrice.replace("$", ""));
+			double size = cost/costPerSqFt;
+			data.homeSquareFeet = String.valueOf(Util.getIntFromDouble(size));
+		}
+		
+	}
+
+}
