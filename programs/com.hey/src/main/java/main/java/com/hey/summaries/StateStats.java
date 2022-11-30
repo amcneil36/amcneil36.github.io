dc8df92f98c9263e007824e36b1881fd26b3fc4a
amcneil36 <andrewemcneil@gmail.com>
2022-11-29 22:32:16 -0500

    fixed up state stats

--- programs/com.hey/src/main/java/main/java/com/hey/summaries/StateStats.java
+++ programs/com.hey/src/main/java/main/java/com/hey/summaries/StateStats.java
@@ -1,101 +1,30 @@
 package main.java.com.hey.summaries;
 
+import java.util.Map;
+
 import main.java.com.hey.CityStats;
-import main.java.com.hey.CityStatsSuper.AndrewStringWriter;
+import main.java.com.hey.summaries.MetroStats.Stats;
 
 public class StateStats extends StateStatsSuper {
 	
 	@Override
-	public void addStuffToStats(StateStatsObj stats, CityStats.Data data) {
-		stats.peoplePerSqMi.addCity(data, data.populationDensity);
-		stats.hottestMonthsHigh.addCity(data, data.hottestMonthsHigh);
-		stats.coldestHigh.addCity(data, data.coldestHigh);
-		stats.numInchesOfRain.addCity(data, data.numInchesOfRain);
-		stats.annualSnowfall.addCity(data, data.annualSnowfall);
-		stats.numSunnyDays.addCity(data, data.numSunnyDays);
-		stats.numDaysOfRain.addCity(data, data.numDaysOfRain);
-		stats.avgSummerDewPoint.addCity(data, data.avgSummerDewPoint);
-		stats.avgAnnualDewPoint.addCity(data, data.avgAnnualDewPoint);
-		stats.avgYearlyWindspeed.addCity(data, data.avgYearlyWindspeed);
-		stats.medianAge.addCity(data, data.medianAge);
-		stats.percentWithAtleastBachelors.addCity(data, data.percentWithAtleastBachelors);
-		stats.medianIncome.addCity(data, data.medianIncome);
-		stats.povertyRate.addCity(data, data.povertyRate);
-		stats.medianHomePrice.addCity(data, data.medianHomePrice);
-		stats.homeSquareFeet.addCity(data, data.homeSquareFeet);
-		stats.costPerSquareFoot.addCity(data, data.costPerSquareFoot);
-		stats.homeOwnershipRate.addCity(data, data.homeOwnershipRate);
-		stats.percentDemocrat.addCity(data, data.percentDemocrat);
-		stats.percentRepublican.addCity(data, data.percentRepublican);
-		stats.percentAsian.addCity(data, data.percentAsian);
-		stats.percentBlack.addCity(data, data.percentBlack);
-		stats.percentWhite.addCity(data, data.percentWhite);
-		stats.percentHispanic.addCity(data, data.percentHispanic);
-		stats.foreignBornPercent.addCity(data, data.foreignBornPercent);
-		stats.uvIndex.addCity(data, data.uvIndex);
-		stats.singlePopulation.addCity(data, data.singlePopulation);
-		stats.fbiViolentCrimeData.addCity(data, data.fbiViolentCrimeRate);
-		stats.fbiPropertyCrimeData.addCity(data, data.fbiPropertyCrimeRate);
-		stats.laborForceParticipationRate.addCity(data, data.laborForceParticipationRate);
-		stats.addDataToMapOfTimeZoneToPopulation(data);
-		int cityPop = Integer.valueOf(data.population);
-		stats.statePopulation += cityPop;
+	public void addStuffToStats(Stats stats, CityStats.Data data) {
+		new MetroStats().addStuffToStats(stats, data);
 
-	}
-	
-	static String startSt = "State Name,Population,People Per Sq Mi,Hottest month's avg high (F),Coldest month's avg high (F),Annual rainfall (in),Annual days of precipitation,Annual days of sunshine,Annual snowfall (in),Avg Summer Dew Point,Avg Annual Dew Point,Average yearly windspeed (mph),"
-			+ "Violent crime index,Property crime index,Median age,% with at least Bachelor's degree,"
-			+ "Median household income,Poverty Rate,Median home price,Median home sqft,"
-			+ "Median home cost per sqft,Homeownership Rate,Population growth since 2010,"
-			+ "% Democrat,% Republican,% Asian,% Black,% Non-Hispanic White,% Hispanic,Foreign Born %,UV Index,Single Population,% of income spent on housing costs (owners),Number of sex offenders per 10k residents,Predominant Timezone,Num Violent Crimes Per 100k residents,Num Property Crimes Per 100k residents,Labor Force Participation rate";
-
-	@Override
-	public String getStartStr() {
-		return startSt;
-	}
-
-	@Override
-	public void addToSb(AndrewStringWriter sb, StateStatsObj stat){
-		sb.appendWA(stat.peoplePerSqMi);
-		sb.appendWA(stat.hottestMonthsHigh);
-		sb.appendWA(stat.coldestHigh);
-		sb.appendWA(stat.numInchesOfRain);
-		sb.appendWA(stat.numDaysOfRain);
-		sb.appendWA(stat.numSunnyDays);
-		sb.appendWA(stat.annualSnowfall);
-		sb.appendWA(stat.avgSummerDewPoint);
-		sb.appendWA(stat.avgAnnualDewPoint);
-		sb.appendWA(stat.avgYearlyWindspeed);
-		sb.appendWA(stat.violentCrime);
-		sb.appendWA(stat.propertyCrime);
-		sb.appendWA(stat.medianAge);
-		sb.appendWAPercent(stat.percentWithAtleastBachelors);
-		sb.appendWADollar(stat.medianIncome);
-		sb.appendWAPercent(stat.povertyRate);
-		sb.appendWADollar(stat.medianHomePrice);
-		sb.appendWA(stat.homeSquareFeet);
-		sb.appendWADollar(stat.costPerSquareFoot);
-		sb.appendWAPercent(stat.homeOwnershipRate);
-		sb.appendWAPercent(stat.populationGrowthSince2010);
-		sb.appendWAPercent(stat.percentDemocrat);
-		sb.appendWAPercent(stat.percentRepublican);
-		sb.appendWAPercent(stat.percentAsian);
-		sb.appendWAPercent(stat.percentBlack);
-		sb.appendWAPercent(stat.percentWhite);
-		sb.appendWAPercent(stat.percentHispanic);
-		sb.appendWAPercent(stat.foreignBornPercent);
-		sb.appendWA(stat.uvIndex);
-		sb.appendWAPercent(stat.singlePopulation);
-		sb.appendWAPercent(stat.percentOfIncomeLostToHousingCosts);
-		sb.appendWA(stat.sexOffenderCount);
-		sb.appendWithComma(stat.getPrimaryTimeZone());
-		sb.appendWA(stat.fbiViolentCrimeData);
-		sb.appendWA(stat.fbiPropertyCrimeData);
-		sb.appendWAPercent(stat.laborForceParticipationRate);
 	}
 	
 	public static void main(String[] args) throws Exception {
 		StateStats ss = new StateStats();
 		ss.performStuff();
+	}
+
+	@Override
+	public String[] getHeader() {
+		return new MetroStats().getHeader();
+	}
+
+	@Override
+	public void extractDataToArray(Stats stat, Map<String, Integer> mapOfNameToIdx, Object[] arr) {
+		new MetroStats().extractDataToArray(stat, mapOfNameToIdx, arr);
 	}
 }
--- programs/com.hey/src/main/java/main/java/com/hey/summaries/StateStatsSuper.java
+++ programs/com.hey/src/main/java/main/java/com/hey/summaries/StateStatsSuper.java
@@ -1,5 +1,8 @@
 package main.java.com.hey.summaries;
 
+import static main.java.com.hey.CityStats.POPULATION;
+import static main.java.com.hey.CityStats.STATE;
+
 import java.io.FileWriter;
 import java.util.ArrayList;
 import java.util.Collections;
@@ -11,11 +14,28 @@ import java.util.Set;
 import main.java.com.hey.CityStats;
 import main.java.com.hey.CityStats.Data;
 import main.java.com.hey.CityStatsSuper.AndrewStringWriter;
+import main.java.com.hey.summaries.MetroStats.Stats;
 
 public abstract class StateStatsSuper {
 	public static class StateStatsObj extends MetroStats.Stats{
 		public int statePopulation = 0;
 		public String stateName = "";
+	}
+	
+	public void addToSb(AndrewStringWriter sb, Stats stat) {
+		
+		Map<String, Integer> mapOfNameToIdx = new HashMap<>();
+		String[] headers = getHeader();
+		Object[] arr = new Object[headers.length];
+		for (int i = 0; i < headers.length; i++) {
+			mapOfNameToIdx.put(headers[i], i);
+		}
+		extractDataToArray(stat, mapOfNameToIdx, arr);
+	
+		
+		for (Object obj : arr) {
+			sb.appendWithComma(obj.toString());
+		}
 	}
 	
 	public void performStuff()throws Exception  {
@@ -30,6 +50,9 @@ public abstract class StateStatsSuper {
 			}
 			StateStatsObj stats = mapOfStateNameToStats.get(state);
 			addStuffToStats(stats, data);
+			stats.addDataToMapOfTimeZoneToPopulation(data);
+			int cityPop = Integer.valueOf(data.population);
+			stats.statePopulation += cityPop;
 		}
 		
 		Set<String> keys = mapOfStateNameToStats.keySet();
@@ -41,7 +64,17 @@ public abstract class StateStatsSuper {
 		String filePath = "C:\\Users\\anmcneil\\amcneil36.github.io\\programs\\StateStats\\StateStats.csv";
 		FileWriter myWriter = new FileWriter(filePath);
 		AndrewStringWriter sb = new AndrewStringWriter();
-		sb.appendLastItem(getStartStr());
+		StringBuilder mySb = new StringBuilder();
+		String[] firstHeaders = {STATE, POPULATION};
+		for (String str : firstHeaders) {
+			mySb.append(str).append(",");
+		}
+		String[] remainingHeaders = getHeader();
+		for (String str : remainingHeaders) {
+			mySb.append(str).append(",");
+		}
+		mySb.deleteCharAt(mySb.length() - 1);
+		sb.appendLastItem(mySb.toString());
 		for (StateStatsObj stat : statsList) {
 			sb.appendWithComma(stat.stateName).appendWithComma(stat.statePopulation);
 			addToSb(sb, stat);
@@ -52,10 +85,10 @@ public abstract class StateStatsSuper {
 		myWriter.close();
 		System.out.println("wrote to file " + filePath);		
 	}
-
-	public abstract void addToSb(AndrewStringWriter sb, StateStatsObj stat);
-
-	public abstract String getStartStr();
-
-	public abstract void addStuffToStats(StateStatsObj stats, Data data);
+	
+	public abstract void addStuffToStats(Stats stats, Data data);
+	
+	public abstract String[] getHeader();
+	
+	public abstract void extractDataToArray(Stats stat, Map<String, Integer> mapOfNameToIdx, Object[] arr);
 }
