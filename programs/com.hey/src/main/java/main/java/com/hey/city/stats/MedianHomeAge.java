e8a85d6b3982691ca826a98526e6bd7d5083405a
amcneil36 <andrewemcneil@gmail.com>
2022-11-30 20:10:55 -0500

    home age

---
+++ programs/com.hey/src/main/java/main/java/com/hey/city/stats/MedianHomeAge.java
@@ -0,0 +1,38 @@
+package main.java.com.hey.city.stats;
+
+import java.util.Map;
+
+import main.java.com.hey.CityStats;
+import main.java.com.hey.us.census.ACS2021DataReader;
+import main.java.com.hey.us.census.ACS2021DataReader.Result;
+
+public class MedianHomeAge extends CityStats {
+
+	private static Map<String, Result> mapOfFipsCodeToResult;
+
+	private static String[] variables = new String[] { "B25035_001E(median year structure built)" };
+
+	// name: B25035_001E; label: estimate!!median year structure built; concept:
+	// median year structure built
+	public static void main(String[] args) throws Exception {
+		mapOfFipsCodeToResult = ACS2021DataReader.getResults(variables);
+		MedianHomeAge ma = new MedianHomeAge();
+		ma.processAllStates();
+
+	}
+
+	@Override
+	protected void updateData(Data data, String stateName) throws Exception {
+		if (mapOfFipsCodeToResult.containsKey(data.fipsCode)) {
+			Result result = mapOfFipsCodeToResult.get(data.fipsCode);
+			String homeAge = result.results.get(variables[0]);
+			int yearHomeBuilt = Integer.valueOf(homeAge);
+			int homeAgeInt = 2020-yearHomeBuilt;
+			if (homeAgeInt > 1 && homeAgeInt < 1000) {
+				data.medianHomeAge = String.valueOf(homeAgeInt);	
+			}
+		}
+
+	}
+
+}
