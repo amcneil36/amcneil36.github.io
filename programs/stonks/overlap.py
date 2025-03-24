61510b2982cd81b7b1aeace3b8d08e3345dcb9c1
amcneil36 <51429671+amcneil36@users.noreply.github.com>
2025-03-24 10:01:34 -0500

    Update overlap.py

--- programs/stonks/overlap.py
+++ programs/stonks/overlap.py
@@ -1,19 +1,14 @@
 import urllib.request
 from html.parser import HTMLParser
 
-class Holding:
-    def __init__(self, symbol: str, weight: float):
-        self.symbol = symbol
-        self.weight = weight
-
 class ETF:
     
-    def _fetch_holdings(self, url: str) -> list[Holding]:
+    def _fetch_holdings(self, url: str) -> dict[str, float]:
         response = urllib.request.urlopen(url)
         html_content = response.read().decode('utf-8')
         lines = html_content.split('\n')
         
-        holdings: list[Holding] = []
+        holdings: dict[str, float] = {}
         for line in lines:
             if len(line) == 0: # last line
                 break
@@ -22,24 +17,19 @@ class ETF:
             if symbol == "-":
                 symbol = parsed_line[0].replace(" ", "_")
             weight = float(parsed_line[2])
-            holding = Holding(symbol, weight)
-            holdings.append(holding)
+            holdings[symbol] = weight
         return holdings
             
     def __init__(self, name: str):
         url = "https://raw.githubusercontent.com/amcneil36/amcneil36.github.io/refs/heads/master/programs/stonks/etfs/" + name + ".csv"
-        self.holdings: list[Holding] = self._fetch_holdings(url)
+        self.holdings: dict[str, float] = self._fetch_holdings(url)
         filename = url.split('/')[-1]
         self.name: str = name
         
-    def get_holdings(self) -> dict[str, float]:
-        """Returns a dictionary with symbols as keys and their respective weights as values."""
-        return {holding.symbol: holding.weight for holding in self.holdings}
-        
 def compute_etf_overlap(etf1: ETF, etf2: ETF) -> float:
     # Get the holdings dictionaries (symbols as keys and weights as values) for both ETFs
-    holdings1 = etf1.get_holdings()
-    holdings2 = etf2.get_holdings()
+    holdings1 = etf1.holdings
+    holdings2 = etf2.holdings
     
     # Find the common symbols between the two ETFs
     common_symbols = set(holdings1.keys()).intersection(holdings2.keys())
