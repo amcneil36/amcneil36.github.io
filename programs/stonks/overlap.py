cb521573a244176a990293949d5eb52fc677a760
amcneil36 <51429671+amcneil36@users.noreply.github.com>
2025-03-24 09:05:09 -0500

    Update overlap.py

--- programs/stonks/overlap.py
+++ programs/stonks/overlap.py
@@ -7,26 +7,27 @@ class Holding:
         self.weight = weight
 
 class ETF:
-    def __init__(self, url: str):
-        self.holdings = fetch_holdings(url)
-        filename = url.split('/')[-1]
-        self.name = filename.split('.')[0]
-
-def fetch_holdings(url: str):
-    response = urllib.request.urlopen(url)
-    html_content = response.read().decode('utf-8')
-    lines = html_content.split('\n')
     
-    holdings: list[Holding] = []
-    for line in lines:
-        if len(line) == 0: # last line
-            break
-        parsed_line = line.split(',')
-        symbol = parsed_line[0]
-        weight = float(parsed_line[1])
-        holding = Holding(symbol, weight)
-        holdings.append(holding)
+    def _fetch_holdings(self, url: str) -> list[Holding]:
+        response = urllib.request.urlopen(url)
+        html_content = response.read().decode('utf-8')
+        lines = html_content.split('\n')
+        
+        holdings: list[Holding] = []
+        for line in lines:
+            if len(line) == 0: # last line
+                break
+            parsed_line = line.split(',')
+            symbol = parsed_line[0]
+            weight = float(parsed_line[1])
+            holding = Holding(symbol, weight)
+            holdings.append(holding)
+        return holdings
+            
+    def __init__(self, url: str):
+        self.holdings: list[Holding] = self._fetch_holdings(url)
+        filename = url.split('/')[-1]
+        self.name: str = filename.split('.')[0]
 
-spy_url = "https://raw.githubusercontent.com/amcneil36/amcneil36.github.io/refs/heads/master/programs/stonks/etfs/spy.csv"
-spy = ETF(spy_url)
+spy = ETF("https://raw.githubusercontent.com/amcneil36/amcneil36.github.io/refs/heads/master/programs/stonks/etfs/spy.csv")
 print(spy.name)
